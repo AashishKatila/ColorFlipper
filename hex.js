919121f28b86715185f38b52a0aa3223435433cb
AashishKatila <aashishkatila@gmail.com>
2022-01-17 21:04:44 +0545

    first commit

---
+++ hex.js
@@ -0,0 +1,17 @@
+const hexColor = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const color = document.querySelector('.color');
+const btn = document.getElementById('btn');
+
+btn.addEventListener("click",function(){
+	
+	let hex = "#";
+	for(let i = 0;i<6;i++){
+		hex += hexColor[getRandomNumber()];
+	}
+	document.body.style.backgroundColor = hex;
+	color.textContent = hex;
+})
+
+function getRandomNumber(){
+	return Math.floor(Math.random()*hexColor.length);
+}
